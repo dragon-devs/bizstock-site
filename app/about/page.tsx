47b42c696f38c93e318e0ee5b3e49ec0913e82ad
Malak Saad <malaksaadsalam@gmail.com>
2025-09-21 16:01:59 +0500

    sys: branch test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,6 +4,7 @@ import NotFound from "@/app/not-found";
 const AboutUs = () => {
   return (
     <div>
+      <div>about</div>
       <NotFound tag="About Us" description="The page you are looking for is under development. Thank you for your patience." />
     </div>
   );
